aeae8bcdd40c27fd1a3d2d709b1f1ffe8cb770e5
Ahmed-Amer01 <ahmedamer42214953@gmail.com>
2025-07-15 21:26:35 +0300

    make Configrations

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { Title } from '@angular/platform-browser';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path:'',
+    //     component:,
+    //     title:
+    // },
+    //
+    // {
+    //     path:'**',
+    //     component: NotFoundComponent,
+    //     title:'Not found'
+    // }
+];
